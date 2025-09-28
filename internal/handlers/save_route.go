97d11ff2b5f3b090a506e5c883344691ce4e2fb4
jack-san-145 <santhos34848550@example.com>
2025-09-28 08:09:38 +0000

    Update save_route.go and fix gitignore

--- internal/handlers/save_route.go
+++ internal/handlers/save_route.go
@@ -27,17 +27,18 @@ func Save_New_route_handler(w http.ResponseWriter, r *http.Request) {
 		// 	admin-app needs to send json like the below format:
 
 			{
-				"up_route_name": "Sattur to Madurai",
-				"down_route_name":"Madurai to Sattur",
+				"up_route_name": "Sattur to Kamaraj-College",
+				"down_route_name":"Kamaraj-College to Sattur",
 				"src":"Sattur",
-				"dest":"Madurai",
+				"dest":"Kamaraj-College",
 				"stops": [
-							{"location_name": "Sattur","lat": "9.3540035",  "lon": "77.9231079","is_stop":true,"arrival_time":"08:30"},
-							{"location_name": "Virudhunagar", "lat": "9.3538361", "lon": "77.9231022","is_stop":true,"arrival_time":"09:15"},
-							{"location_name": "Thirumangalam","lat": "9.3538282",  "lon": "77.9231091","is_stop":false,"arrival_time":"09:45"},
-							{"location_name": "Madurai","lat": "9.353818",  "lon": "77.9231322","is_stop":true,"arrival_time":"10:50"}
+							{"location_name": "Sattur","lat": "9.3540035",  "lon": "77.9231079","is_stop":true,"arrival_time":"08:00"},
+							{"location_name": "rr nagar", "lat": "9.3538361", "lon": "77.9231022","is_stop":true,"arrival_time":"08:15"},
+                            {"location_name": "soolakrai", "lat": "9.3538361", "lon": "77.9231022","is_stop":false,"arrival_time":"08:35"},
+                            {"location_name": "collectrate", "lat": "9.3538361", "lon": "77.9231022","is_stop":true,"arrival_time":"08:45"},
+							{"location_name": "Kamaraj-College","lat": "9.3538282",  "lon": "77.9231091","is_stop":false,"arrival_time":"09:00"}
 						],
-				"departure_time":"16:20",
+				"down_departure_time":"16:40"
 			}
 	*/
 }
