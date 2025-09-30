6fd995dd758bc0dcd83566c8dcba27831bc491fd
jack-san-145 <santhos34848550@example.com>
2025-09-30 15:35:49 +0000

    validate the admin's login with redis

--- internal/handlers/admin_handler.go
+++ internal/handlers/admin_handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"yus/internal/services"
+	"yus/internal/storage/redis"
 )
 
 func Admin_registerhandler(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +22,22 @@ func Admin_registerhandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		admin_register_status["status"] = "invalid"
 	}
+}
 
+func Admin_login_handler(w http.ResponseWriter, r *http.Request) {
+	var login_status = make(map[string]string)
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("error while parsing admin login form")
+		login_status["login_status"] = "invalid"
+	} else {
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		if redis.Validate_Admin_login(email, password) {
+			login_status["login_status"] = "valid"
+		} else {
+			login_status["login_status"] = "invalid"
+		}
+	}
+	WriteJSON(w, r, login_status)
 }
--- internal/storage/redis/admin.go
+++ internal/storage/redis/admin.go
@@ -8,9 +8,26 @@ import (
 
 func StoreAdmin(name string, email string, password string) {
 	hased_pass := services.Hash_this_password(password)
-	err := rc.HSet(context.Background(), "driver-name", name, "driver-email", email, "password", hased_pass).Err()
+	err := rc.HSet(context.Background(), "Admin-data", "name", name, "email", email, "password", hased_pass).Err()
 	if err != nil {
 		fmt.Println("error while set the admin details to the redis - ", err)
 		return
 	}
+}
+
+func Validate_Admin_login(email string, password string) bool {
+	password = services.Hash_this_password(password)
+	value, err := rc.HMGet(context.Background(), "Admin-data", email, password).Result() //to get the multiple values in a single query
+	if err != nil {
+		fmt.Println("error while accessing the Admin-data - ", err)
+		return false
+	}
+	rc_email := value[0].(string)
+	rc_password := value[1].(string)
+
+	if rc_email == email && services.Is_password_matched(rc_password, password) {
+		return true
+	}
+	return false
+
 }
