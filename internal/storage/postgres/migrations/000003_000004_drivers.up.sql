a13333a7d38ad265f7a3059a0c6b6fd8f8c3a7bd
jack-san-145 <santhos34848550@example.com>
2025-10-02 12:05:21 +0000

    altered the driver id as it should start from 1001

--- internal/storage/postgres/migrations/000003_000004_drivers.up.sql
+++ internal/storage/postgres/migrations/000003_000004_drivers.up.sql
@@ -2,6 +2,9 @@ CREATE TABLE IF NOT EXISTS drivers (
     driver_id SERIAL PRIMARY KEY,
     name TEXT NOT NULL DEFAULT '',
     mobile_no TEXT UNIQUE NOT NULL DEFAULT '',
-    email text UNIQUE NOT NULL DEFAULT '',
+    email TEXT UNIQUE NOT NULL DEFAULT '',
     password TEXT NOT NULL DEFAULT ''
 );
+
+-- Change the sequence to start from 1001
+ALTER SEQUENCE drivers_driver_id_seq RESTART WITH 1001;
