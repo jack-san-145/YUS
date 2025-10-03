4973310ca97c66a05f6fa22dbe37e24d5dd3676d
jack-san-145 <santhosh34848550@gmail.com>
2025-10-03 14:36:37 +0000

    Set default value for driver_id through migrations

--- internal/storage/postgres/migrations/000004_current_bus_route.up.sql
+++ internal/storage/postgres/migrations/000004_current_bus_route.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS current_bus_route (
     bus_id int PRIMARY KEY,
-    driver_id int NOT NULL,
+    driver_id int NOT NULL DEFAULT 1000,
     route_id int NOT NULL DEFAULT 0,
     name TEXT NOT NULL DEFAULT '',
     src TEXT NOT NULL DEFAULT '',
