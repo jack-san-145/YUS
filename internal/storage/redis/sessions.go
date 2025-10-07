76dc15c8777faaa8c7ca72bef7f34b14127f9d2b
jack-san-145 <santhosh34848550@gmail.com>
2025-10-07 18:14:40 +0000

    created driver sessions in redis

--- internal/storage/redis/sessions.go
+++ internal/storage/redis/sessions.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -16,6 +17,20 @@ func Create_Driver_Session(driver_id int) {
 	err := rc.HSet(context.Background(), session_id, "category", "DRIVER", "driver_id", driver_id).Err()
 	if err != nil {
 		fmt.Println("error while setting the new driver session to the redis - ", err)
-
 	}
+
+	//set TTL for session
+	err = rc.Expire(context.Background(), session_id, 7*24*time.Hour).Err()
+	if err != nil {
+		fmt.Println("error while setting ttl to session - ", err)
+	}
+}
+
+func Get_Driver_session(session_id string) string {
+	driver_id, err := rc.HGet(context.Background(), session_id, "driver_id").Result()
+	if err != nil {
+		fmt.Println("error while getting the driver session from redis - ", err)
+	}
+	return driver_id
+
 }
