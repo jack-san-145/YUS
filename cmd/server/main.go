1bdf98eed8348acee37d91071dfbcc769725bc25
jack-san-145 <santhosh34848550@gmail.com>
2025-11-06 14:03:53 +0000

    organised the main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	// "path/filepath"
-
 	"yus/internal/handlers"
 	"yus/internal/storage/postgres"
 	"yus/internal/storage/redis"
@@ -38,23 +35,59 @@ func main() {
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
 	router.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
-	//html pages
-	router.Get("/yus/serve-index-page", handlers.Serve_index_page)
+	//web pages
 	router.Get("/", handlers.Serve_logo_page)
+	router.Get("/yus/serve-index-page", handlers.Serve_index_page)
 	router.Get("/yus/serve-login-page", handlers.Serve_login_page)
-
 	router.Get("/yus/serve-bus-schedule-page", handlers.Serve_bus_schedule_page)
 	router.Get("/yus/serve-driver-page", handlers.Serve_driver_page)
 	router.Get("/yus/serve-register-page", handlers.Serve_register_page)
 	router.Get("/yus/serve-otp-verify-page", handlers.Serve_otp_verify_page)
 
-	router.Put("/yus/change-route/{direction}", handlers.ChangeRoute_direction_handler)
+	/*
 
-	router.Post("/yus/save-new-route", handlers.Save_New_route_handler)
-	router.Post("/yus/add-new-driver", handlers.Add_new_driver_handler)
+
+
+		admin-operations(website)
+
+	*/
+	router.Post("/yus/admin-login", handlers.Admin_login_handler)
 	router.Post("/yus/send-otp-admin", handlers.Admin_otp_handler)
 	router.Post("/yus/verify-otp-admin", handlers.Verify_admin_otp)
+	router.Put("/yus/change-route/{direction}", handlers.ChangeRoute_direction_handler)
+
+	router.Post("/yus/add-new-driver", handlers.Add_new_driver_handler)
+	router.Get("/yus/get-available-routes", handlers.Load_all_available_routes)
+
+	//yus.kwscloud.in/yus/add-new-bus?bus_id=10
+	router.Put("/yus/add-new-bus", handlers.Add_New_Bus_handler)
+
+	//yus/allocate-bus-route?route_id=42&bus_id=10
+	router.Put("/yus/allocate-bus-route", handlers.Map_Route_With_Bus_handler)
+	router.Post("/yus/allocate-bus-driver", handlers.Map_Driver_With_Bus_handler)
+	router.Get("/yus/get-available-drivers", handlers.Load_all_available_drivers)
+
+	//yus.kwscloud.in/yus/get-cached-routes?bus_id=10
+	router.Get("/yus/get-cached-routes", handlers.Cached_route_handler)
+	router.Get("/yus/get-current-schedule", handlers.Get_Schedule_handler)
+
+	/*
+
+
+
+		admin-operations(mobile)
+
+	*/
 	router.Post("/yus/admin-login", handlers.Admin_login_handler)
+	router.Post("/yus/save-new-route", handlers.Save_New_route_handler)
+
+	/*
+
+
+
+		passenger-operations
+
+	*/
 	router.Get("/yus/passenger-ws", handlers.Passenger_Ws_handler)
 	router.Get("/yus/src-{source}&dest-{destination}", handlers.Src_Dest_handler) //here i changed the endpoint format
 
@@ -63,24 +96,13 @@ func main() {
 	//yus.kwscloud.in/yus/get-route?bus_id={bus_id}
 	router.Get("/yus/get-route", handlers.Get_rotue_by_busID)
 
-	router.Get("/yus/get-available-routes", handlers.Load_all_available_routes)
+	/*
 
-	//yus.kwscloud.in/yus/add-new-bus?bus_id=10
-	router.Put("/yus/add-new-bus", handlers.Add_New_Bus_handler)
 
-	//yus/allocate-bus-route?route_id=42&bus_id=10
-	router.Put("/yus/allocate-bus-route", handlers.Map_Route_With_Bus_handler)
 
-	router.Post("/yus/allocate-bus-driver", handlers.Map_Driver_With_Bus_handler)
+		driver-operations
 
-	router.Get("/yus/get-available-drivers", handlers.Load_all_available_drivers)
-
-	//yus.kwscloud.in/yus/get-cached-routes?bus_id=10
-	router.Get("/yus/get-cached-routes", handlers.Cached_route_handler)
-
-	router.Get("/yus/get-current-schedule", handlers.Get_Schedule_handler)
-
-	//driver
+	*/
 
 	router.Post("/yus/send-otp-driver-password", handlers.Driver_Otp_handler)
 	router.Post("/yus/verify-otp-driver-password", handlers.Verify_driver_otp)
@@ -91,6 +113,14 @@ func main() {
 
 	//yus.kwscloud.in/yus/get-allotted-bus
 	router.Get("/yus/get-allotted-bus", handlers.Alloted_bus_handler) //by sessions
+
+	/*
+
+
+
+
+
+	 */
 
 	postgres.Connect()        //make a connection to postgres
 	redis.CreateRedisClient() //made a redis client
