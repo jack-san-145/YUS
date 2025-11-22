207057cce758cd25c01eddf4b899feada158f5db
jack-san-145 <santhosh34848550@gmail.com>
2025-11-22 18:57:52 +0530

    fixed bus in removal operations

--- internal/storage/postgres/deletion.go
+++ internal/storage/postgres/deletion.go
@@ -7,9 +7,10 @@ import (
 
 func Remove_route(route_id int) map[string]bool {
 	query := fmt.Sprintf(`update current_bus_route set route_id=0, direction='', route_name='', src='', dest='' where route_id = %d;
+							delete from cached_bus_route where route_id = %d;
 							delete from route_stops where route_id = %d;  
 							delete from all_routes where route_id = %d; 
-						`, route_id, route_id, route_id)
+						`, route_id, route_id, route_id, route_id)
 	_, err := pool.Exec(context.Background(), query)
 	if err != nil {
 		fmt.Println("error while deleting route - ", err)
@@ -19,8 +20,11 @@ func Remove_route(route_id int) map[string]bool {
 }
 
 func Remove_bus(bus_id int) map[string]bool {
-	query := "delete from current_bus_route where bus_id = $1"
-	_, err := pool.Exec(context.Background(), query, bus_id)
+
+	query := fmt.Sprintf(`delete from cached_bus_route where bus_id = %d;
+							delete from current_bus_route where bus_id = %d;
+						`, bus_id, bus_id)
+	_, err := pool.Exec(context.Background(), query)
 	if err != nil {
 		fmt.Println("error while removing bus - ", err)
 		return map[string]bool{"removed": false}
