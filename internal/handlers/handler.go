63d93d139591de803d791a6e93a401904dac5cbd
jack-san-145 <santhosh34848550@gmail.com>
2025-12-09 12:13:31 +0530

    Isolate Admin handlers

---
+++ internal/handlers/handler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"yus/internal/AppPkg"
+	"yus/internal/storage"
+)
+
+// func Store() *storage.Store {
+// 	return AppPkg.App.Store
+// }
+
+type AdminHandler interface {
+}
+
+type DriverHandler interface {
+}
+
+type PassengerHandler interface {
+}
+
+type YUSHandler struct {
+	Store *storage.Store
+	// it the store that contains both redis and postgres that can be used anywhere in the handlers package with "Store"
+
+	Admin  AdminHandler
+	Driver DriverHandler
+	Pass   PassengerHandler
+}
+
+func NewHandler() *YUSHandler {
+	return &YUSHandler{
+		Store: AppPkg.NewApplication().Store,
+	}
+}
