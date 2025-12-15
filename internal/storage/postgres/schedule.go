32c913524296e234c9fc405e8da6efd67f48b8a2
jack-san-145 <santhosh34848550@gmail.com>
2025-12-15 14:28:10 +0530

    Fix schedule bus db function with batch queries

--- internal/storage/postgres/schedule.go
+++ internal/storage/postgres/schedule.go
@@ -4,27 +4,39 @@ import (
 	"context"
 	"log"
 	"yus/internal/models"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func ScheduleBus(schedule *models.CurrentSchedule) error {
 
-	query := `update current_bus_route set driver_id=1000 where driver_id=$1;
-			  update current_bus_route set route_id=0 where route_id=$2;
-			  update current_bus_route set driver_id=$3 where bus_id=$4;
-			  update current_bus_route set route_id=$5 where bus_id=$6;
-			`
-
-	_, err := pool.Exec(context.Background(), query,
-		schedule.DriverId,
-		schedule.RouteId,
-		schedule.DriverId,
-		schedule.BusId,
-		schedule.RouteId,
-		schedule.BusId,
-	)
-
+	ctx := context.Background()
+	tx, err := pool.Begin(ctx) //transaction for atomic operation
 	if err != nil {
-		log.Println("error while schedule bus - ", err)
+		log.Println("failed to begin tx:", err)
+		return err
+	}
+	defer tx.Rollback(ctx) // safe rollback if commit doesn't happen
+
+	batch := &pgx.Batch{}
+
+	batch.Queue("update current_bus_route set driver_id=1000 where driver_id=$1;", schedule.DriverId)
+	batch.Queue("update current_bus_route set route_id=0 where route_id=$1;", schedule.RouteId)
+	batch.Queue("update current_bus_route set driver_id=$1,route_id=$2 where bus_id=$3;", schedule.DriverId, schedule.RouteId, schedule.BusId)
+
+	br := tx.SendBatch(ctx, batch)
+	defer br.Close()
+
+	for i := 0; i < batch.Len(); i++ {
+		_, err := br.Exec()
+		if err != nil {
+			log.Println("error while scheduling bus routes - ", err)
+			return err
+		}
+	}
+	err = tx.Commit(ctx) //close transaction
+	if err != nil {
+		log.Println("commit failed:", err)
 		return err
 	}
 
