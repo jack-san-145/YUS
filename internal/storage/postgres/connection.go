0692db7e06281934c48f81abca0b08fe8225c7c5
jack-san-145 <santhosh34848550@gmail.com>
2025-12-16 21:09:28 +0530

    Create a PgStore for pool connections and made a connection

--- internal/storage/postgres/connection.go
+++ internal/storage/postgres/connection.go
@@ -21,7 +21,8 @@ func NewPgStore() *PgStore {
 	return &PgStore{}
 }
 
-func Connect() error {
+func (pg *PgStore) Connect(ctx context.Context) error {
+
 	var (
 		config *pgxpool.Config
 		err    error
@@ -33,11 +34,6 @@ func Connect() error {
 	port := os.Getenv("POSTGRES_DB_PORT")
 
 	conn := fmt.Sprintf(`postgres://%s:%s@postgres.kws.services:%s/yus?sslmode=disable`, username, pass, port)
-	// fmt.Println("conn - ", conn)
-
-	//here context is used for timeout -> if the connection try to connect the db for more than 5 sec , it automatticaly close only that connection
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel() // to cancel all the timing after this function ends
 
 	//  ParseConfig parse this "postgres://username:password@host:port/dbname" -> the pool config object so that it can set the maxConnections like that
 	config, err = pgxpool.ParseConfig(conn)
@@ -49,13 +45,13 @@ func Connect() error {
 	config.MaxConns = 10               // seting 10 maximum connections at the time
 	config.MaxConnLifetime = time.Hour //maximum time can the connection live
 
-	pool, err = pgxpool.NewWithConfig(ctx, config) // created the new pool connection with the config
+	pg.Pool, err = pgxpool.NewWithConfig(ctx, config) // created the new pool connection with the config
 	if err != nil {
 		fmt.Println("error while creating the new pool connection - ", err)
 		return err
 	}
 
-	err = pool.Ping(ctx)
+	err = pg.Pool.Ping(ctx)
 	if err != nil {
 		fmt.Println("error while pool connection ping - ", err)
 		return err
@@ -64,6 +60,6 @@ func Connect() error {
 	return nil
 }
 
-func GivePostgresConnection() *pgxpool.Pool {
-	return pool
-}
+// func GivePostgresConnection() *pgxpool.Pool {
+// 	return pool
+// }
--- internal/storage/postgres/deletion.go
+++ internal/storage/postgres/deletion.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 )
 
-func Remove_route(route_id int) error {
+func RemoveRoute(ctx context.Context, routeID int) error {
 	query := fmt.Sprintf(`update current_bus_route set route_id=0, direction='', route_name='', src='', dest='' where route_id = %d;
 							delete from cached_bus_route where route_id = %d;
 							delete from route_stops where route_id = %d;  
 							delete from all_routes where route_id = %d; 
-						`, route_id, route_id, route_id, route_id)
-	_, err := pool.Exec(context.Background(), query)
+						`, routeID, routeID, routeID, routeID)
+	_, err := pool.Exec(ctx, query)
 	if err != nil {
 		fmt.Println("error while deleting route - ", err)
 		return err
