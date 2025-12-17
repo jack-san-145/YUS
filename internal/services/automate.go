03d846b58e7700e6f381184a750f16f5d8790cf7
jack-san-145 <santhosh34848550@gmail.com>
2025-12-17 15:01:12 +0530

    Integrate route automation as a service

---
+++ internal/services/automate.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"yus/internal/AppPkg"
+
+	"github.com/robfig/cron/v3"
+)
+
+func AutomateRouteScheduling(app *AppPkg.Application) {
+
+	c := cron.New(
+		cron.WithSeconds(),
+		cron.WithLocation(time.Local))
+
+	//for 12 AM
+	c.AddFunc("0 0 0 * * *", func() {
+
+		app.Store.DB.ChangeRouteDirection(context.Background(), "UP")
+		current_route, _ := app.Store.DB.GetCurrentBusRoutes(context.Background())
+		app.Store.InMemoryDB.CacheBusRoute(context.Background(), current_route) //cache current routes in redis
+	})
+
+	//for 12 PM
+	c.AddFunc("0 0 12 * * *", func() {
+		app.Store.DB.ChangeRouteDirection(context.Background(), "DOWN")
+		current_route, _ := app.Store.DB.GetCurrentBusRoutes(context.Background())
+		app.Store.InMemoryDB.CacheBusRoute(context.Background(), current_route) //cache current routes in redis
+	})
+	c.Start()
+
+	fmt.Println("Route scheduling started..")
+
+	select {} //it blocks the go routiune and run forever
+}
