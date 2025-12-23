a11d34450082de23279710f7a06b38de38daffc1
jack-san-145 <santhosh34848550@gmail.com>
2025-12-23 12:26:56 +0530

    Create db migration for drival removal request

---
+++ internal/storage/postgres/migrations/000006_driver_removal_request.down.sql
@@ -0,0 +1,1 @@
+drop table IF EXISTS driver_removal_request;
---
+++ internal/storage/postgres/migrations/000006_driver_removal_request.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE driver_removal_request (
+    driver_id INT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
